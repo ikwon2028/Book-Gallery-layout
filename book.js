95606b4a4620345d1000c8f80dbe790a800033fb
ikwon2028 <82852077+ikwon2028@users.noreply.github.com>
2023-05-14 16:24:57 +0900

    Initial commit

---
+++ book.js
@@ -0,0 +1,8 @@
+// 1. https://getbootstrap.com/docs/5.0/content/images/#responsive-images
+
+// 2. https://getbootstrap.com/docs/5.0/utilities/sizing/
+
+// https://getbootstrap.com/docs/5.1/layout/columns/#offset-classes
+
+// https://getbootstrap.com/docs/5.0/utilities/sizing/
+
